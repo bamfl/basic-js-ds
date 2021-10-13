0b571bc04d2a1a6fc8803c3a9cc21a60fef9ae62
Dmitriy Petukhov <pdnsite@gmail.com>
2021-10-13 15:00:48 +0300

    feat: add stack class

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+	constructor() {
+		this.body = [];
+	}
+
+  push(element) {
+    this.body.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		let lastElem = this.body.pop();
+
+		return lastElem;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.body[this.body.length - 1];
   }
-}
+};
