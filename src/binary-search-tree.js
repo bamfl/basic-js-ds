c6f0c362c06444687e42c11aefdf09561989c0c3
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-10-14 10:46:17 +0300

    feat: add min and max methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,12 +1,21 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { Node } = require('../extensions/list-tree.js');
+// const { Node } = require('../extensions/list-tree.js');
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 module.exports = class BinarySearchTree {
+// class BinarySearchTree {
   constructor() {
     this.root = null;
   }
@@ -20,28 +29,27 @@ module.exports = class BinarySearchTree {
       this.root = new Node(data);
     } else {
       let node = this.root;
-      let newNode = new Node(data);
 
       while (node) {
-        if (data > node) {
-          if (!node.right) {
-            break;
-          }
-
+				if (data === node.data) {
+					break;
+				} else if (data > node.data) {
+					if (!node.right) {
+						break;
+					}
           node = node.right;
         } else {
-          if (!node.left) {
-            break;
-          }
-					
+					if (!node.left) {
+						break;
+					}
           node = node.left;
         }
       }
 
-      if (data > node.value) {
-        node.right = newNode;
+      if (data > node.data) {
+        node.right = new Node(data);
       } else {
-        node.left = newNode;
+        node.left = new Node(data);
       }
     }
   }
@@ -54,39 +62,40 @@ module.exports = class BinarySearchTree {
         if (!node.right) {
           return false;
         }
-				return true;
+        return true;
       } else {
         if (!node.left) {
           return false;
         }
-				return true;
+        return true;
       }
     }
   }
 
   find(data) {
-		let node = this.root;
+    let node = this.root;
 
     while (node) {
       if (data > node) {
         if (!node.right) {
           return node.right;
         }
-				return true;
+        return true;
       } else {
         if (!node.left) {
           return null;
         }
-				return node.left;
+        return node.left;
       }
     }
-	}
+  }
 
   remove(data) {
-	}
+    return true;
+  }
 
   min() {
-		if (!this.root) {
+    if (!this.root) {
       return null;
     }
 
@@ -97,10 +106,10 @@ module.exports = class BinarySearchTree {
     }
 
     return node.data;
-	}
+  }
 
   max() {
-		if (!this.root) {
+    if (!this.root) {
       return null;
     }
 
@@ -111,5 +120,15 @@ module.exports = class BinarySearchTree {
     }
 
     return node.data;
-	}
-};
+  }
+}
+
+// const tree = new BinarySearchTree();
+
+// tree.add(10);
+// tree.add(12);
+// tree.add(1);
+// tree.add(1);
+// tree.add(15);
+
+// console.log(tree);
